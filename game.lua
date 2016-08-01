d127986b2749fe146d3b38aa6324d528c2ac2d9b
André Andrade <me@ardydo.com>
2016-08-01 17:16:46 -0300

    meep?

--- game.lua
+++ game.lua
@@ -332,7 +332,6 @@ function scene:destroy( event )
 
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
-    removeEventListener( "enterFrame", step )
 
 end
 
